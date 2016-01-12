b5636fa63814eebfd665289fdbe0e565666bd4d2
kokowin18 <kkwp4b@mail.missouri.edu>
2016-01-11 16:05:23 -0800

    Menu, Blocks and Form

--- magento/app/code/local/Pfay/Test/controllers/IndexController.php
+++ magento/app/code/local/Pfay/Test/controllers/IndexController.php
@@ -2,14 +2,35 @@
 
 class Pfay_Test_IndexController extends Mage_Core_Controller_Front_Action
 {
-   public function indexAction()
-   {
+  public function indexAction()
+  {
      //echo "test index";
-	$this->loadLayout();
+    $this->loadLayout();
     $this->renderLayout();
-   }
-   public function mamethodeAction()
-   {
+  }
+  
+  public function mamethodeAction()
+  {
      echo 'test mymethod';
-    }
+  }
+
+  public function saveAction()
+  {
+    $nom = ''.$this->getRequest()->getPost('nom');
+    $prenom = ''.$this->getRequest()->getPost('prenom');
+    $telephone = ''.$this->getRequest()->getPost('telephone');
+
+    if(isset($nom)&&($nom!='') && isset($prenom)&&($prenom!='')
+                               && isset($telephone)&&($telephone!='') )
+   {
+      
+      $contact = Mage::getModel('test/test');
+      $contact->setData('nom', $nom);
+      $contact->setData('prenom', $prenom);
+      $contact->setData('telephone', $telephone);
+      $contact->save();
+   }
+    $this->_redirect('*/');
+  }
 }
+
